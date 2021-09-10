e4cd9991be409eb9bc73f840eacbe16cdf01af27
DonDejvo <dada542cz@gmail.com>
2021-09-11 01:26:23 +0200

    switched to minified files

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import * as Lancelot from "https://cdn.jsdelivr.net/gh/DonDejvo/Lancelot/core.js";
+import * as Lancelot from "https://cdn.jsdelivr.net/gh/DonDejvo/Lancelot/core.min.js";
 
 class Game {
     constructor() {
